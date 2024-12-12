9abd7fa3dc7a45ff97519915bd38b4b25774e274
ziyoxon <mirziyoxonm@gmail.com>
2024-12-12 11:12:42 +0500

    imgs

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+        <h1 className='text-center'>Welcome Children</h1>
+        <img src="src/assets/sardor.jpg" alt="" />
+      </div>
+    </>
+  )
+}
+
+export default App
